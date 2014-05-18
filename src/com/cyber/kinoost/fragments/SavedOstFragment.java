d1026f1ff5db848e84175cfd5b595a5d6512894d
khanshu <khanshu@hotmail.com>
2014-05-19 00:31:02 +0400

    в процессе

--- src/com/cyber/kinoost/fragments/SavedOstFragment.java
+++ src/com/cyber/kinoost/fragments/SavedOstFragment.java
@@ -1,9 +1,11 @@
 package com.cyber.kinoost.fragments;
 
 import java.io.File;
+
 import java.sql.SQLException;
 import java.util.List;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Html;
@@ -11,89 +13,87 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.cyber.kinoost.R;
 import com.cyber.kinoost.adapters.OstListViewAdapter;
+import com.cyber.kinoost.adapters.FilmOstListViewAdapter;
+import com.cyber.kinoost.api.Account;
+import com.cyber.kinoost.api.ApiHelper;
+import com.cyber.kinoost.api.vk.sources.Api;
 import com.cyber.kinoost.db.models.Music;
 import com.cyber.kinoost.db.repositories.MusicRepository;
 
 public class SavedOstFragment extends Fragment {
 
-
-
-
-
-
+	private Context mContext;
+	final ApiHelper apiHelper = new ApiHelper();
+	
 	public SavedOstFragment() {
 		// Empty constructor required for fragment subclasses
 	}
-	
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		
-		View saved = inflater.inflate(R.layout.fragment_ost,
-				container, false);
-	
-		
-		/*String songName;
-    	try {
-    		songName = (String) getArguments().getSerializable("songName");
-    	} catch(Exception e) {
-    		songName = "";    		
-    	}  	*/
-    	
-    	
-    	List<Music> music = getMusic();
-    	
-    	ListView listView = (ListView) saved.findViewById(R.id.listView);
-        listView.setAdapter(new OstListViewAdapter(getActivity(), music));
-   	
-        return saved;
-    }
-    
-    private List<Music> getMusic()  {
-    	
-    	List<Music> music = null;
-    	MusicRepository musicRepo = new MusicRepository(getActivity().getBaseContext());
-		final String MEDIA_PATH = new String(getActivity().getCacheDir().toString());
-		
-	    File home = new File(MEDIA_PATH);
-	    
 
+		View saved = inflater.inflate(R.layout.fragment_ost, container, false);
 
-	    if (home.listFiles(new FileExtensionFilter()).length > 0) {
-	        for (File file : home.listFiles(new FileExtensionFilter())) {
-	            String title = file.getName().substring(0, (file.getName().length() - 4));
-	            String path = file.getPath();
+		/*
+		 * String songName; try { songName = (String)
+		 * getArguments().getSerializable("songName"); } catch(Exception e) {
+		 * songName = ""; }
+		 */
 
-	            try {
+		final List<Music> music = getMusic();
+
+		ListView listView = (ListView) saved.findViewById(R.id.listView);
+		listView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+		Log.i("ListAdapter", music.get(1).getName());
+			Account account = new Account(mContext);
+				Api api = new Api(account);
+
+				apiHelper.getSongMusic(mContext, api, music.get(1));
+
+            }
+        });
+		listView.setAdapter(new OstListViewAdapter(getActivity(), music));
+		return saved;
+	}
+
+	private List<Music> getMusic() {
+
+		List<Music> music = null;
+		MusicRepository musicRepo = new MusicRepository(getActivity()
+				.getBaseContext());
+		final String MEDIA_PATH = new String(getActivity().getCacheDir()
+				.toString());
+
+		File home = new File(MEDIA_PATH);
+
+		if (home.listFiles(new FileExtensionFilter()).length > 0) {
+			for (File file : home.listFiles(new FileExtensionFilter())) {
+				String title = file.getName().substring(0,
+						(file.getName().length() - 4));
+				String path = file.getPath();
+
+				try {
 					music = musicRepo.findMusicByName(title, 0, 30);
 				} catch (SQLException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-	            
-    	
-	        }}
-    	
-    	/*MusicRepository musicRepo = new MusicRepository(getActivity().getBaseContext());
-    	List<Music> music = null;
-    	try {
-			music = musicRepo.findMusicByName(songName, 0, 30);
-			Log.d("SIZE", Integer.toString(music.size()));
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-    	return music;
-    
-		
-	
-	
+
+			}
+		}
+
+
+		return music;
+
 	}
 
 }
